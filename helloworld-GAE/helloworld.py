604bb986f3b66345e25f5b8c5a1343f8905418cd
Greg Burek <greg.burek+dev@gmail.com>
2011-11-15 02:18:13 +0000

    Typo fixes

--- helloworld-GAE/helloworld.py
+++ helloworld-GAE/helloworld.py
@@ -63,12 +63,12 @@ class Guestbook(webapp.RequestHandler):
         guestbook_name = self.request.get('guestbook_name')
         greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-        if users.get_current.user():
+        if users.get_current_user():
             greeting.author = users.get_current_user()
 
         greeting.content = self.request.get('content')
         greeting.put()
-        self.redirect('/?' + urllib.encode({'guestbook_name': guestbook_name}))
+        self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
 
 
 application = webapp.WSGIApplication([('/', MainPage),
